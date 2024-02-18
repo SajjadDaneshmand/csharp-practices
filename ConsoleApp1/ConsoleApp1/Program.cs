983e575c7db5c9b6e4361f029d4fcfa19242baf5
unknown <Mr.SajjadDaneshmand@gmail.com>
2024-02-18 15:15:56 +0330

    pactice one

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number 1: ");
+            string user_num = Console.ReadLine();
+            int num1 = System.Convert.ToInt32(user_num);
+            Console.WriteLine("Please enter number 2: ");
+            string user_num2 = Console.ReadLine();
+            int num2 = System.Convert.ToInt32(user_num2);
+            int sum_nums = num1 + num2;
+            Console.WriteLine("your final num is: " + sum_nums);
+            Console.ReadKey();
+        }
+    }
+}
