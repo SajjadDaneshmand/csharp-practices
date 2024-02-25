a27064bf57d2eb1e08bf4635310185d525720462
unknown <Mr.SajjadDaneshmand@gmail.com>
2024-02-25 12:15:02 +0330

    report and practice

---
+++ practice-one/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give me the number of persons: ");
+            string inputData = Console.ReadLine();
+            int personNumber = Convert.ToInt32(inputData);
+            Person[] peaple = new Person[personNumber];
+            for (int i = 0; i < personNumber; i++)
+            {
+                Console.WriteLine("enter name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("enter family:");
+                string family = Console.ReadLine();
+                Console.WriteLine("enter age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Person person = new Person(name, family, age);
+                peaple[i] = person;
+            }
+
+            Console.WriteLine("here is the result:");
+
+            foreach (Person human in peaple)
+            {
+                Console.WriteLine($"name: {human.name}");
+                Console.WriteLine($"family: {human.family}");
+                Console.WriteLine($"age: {human.age}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
