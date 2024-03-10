f90779490749996de1f5fa2b47a177d3277ef9f0
unknown <Mr.SajjadDaneshmand@gmail.com>
2024-03-10 18:10:34 +0330

    .\IBank.cs

---
+++ IBank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    interface IBank
+    {
+        bool Deposit(double amount);
+        bool withdraw(double amount);
+        string DisplayDetails();
+
+    }
+}
